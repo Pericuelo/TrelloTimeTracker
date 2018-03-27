cb19624ee2fedb0aa84e5a0e2f50ef74b843236a
Pere Fonolleda <pere@strangelightgames.com>
2018-03-27 18:33:55 +0200

    Copiada estructura del tuto de trello

--- public/js/client.js
+++ public/js/client.js
@@ -5,6 +5,12 @@ var Promise = TrelloPowerUp.Promise;
 var BLACK_ROCKET_ICON = 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421';
 
 TrelloPowerUp.initialize({
+  'card-buttons': function(t, options){
+      return [{
+        icon: 'https://github.com/Pericuelo/TrelloTimeTracker/blob/master/assets/icon.png',
+        text: 'Time Track',
+      }];
+    },
   // Start adding handlers for your capabilities here!
 	// 'card-buttons': function(t, options) {
 	// 	return [{
