f4e3da41b25bab8ca67be12c46f37bc48ccdefb7
Pere Fonolleda <pere@strangelightgames.com>
2018-03-27 18:45:07 +0200

    Canviada configuració per a que surti el botó

--- server.js
+++ server.js
@@ -4,17 +4,17 @@ var cors = require('cors');
 var app = express();
 
 // your manifest must have appropriate CORS headers, you could also use '*'
-app.use(cors({ origin: '*' }));
+app.use(cors({ origin: 'https://trello.com' }));
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("*", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+  response.sendFile(__dirname + '/public/index.html');
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+// var listener = app.listen(process.env.PORT, function () {
+//   console.log('Your app is listening on port ' + listener.address().port);
+// });
